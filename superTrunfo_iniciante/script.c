c09f291ec423f6f7b439654d8b2d6b8b4181076b
Davi Vidal Rogerio de Paula <davi06vidal@gmail.com>
2026-02-05 12:51:16 +0000

    refiz o codigo inteiro iniciante

--- superTrunfo_iniciante/script.c
+++ superTrunfo_iniciante/script.c
@@ -1,82 +1,59 @@
 #include <stdio.h>
 
-typedef struct
+struct Carta
 {
     char codigo[4];
     int populacao;
     float area;
     float pib;
     int pontosTuristicos;
-    float densidade;
-    float pibPerCapita;
-
-} Carta;
+};
 
 int main()
 {
-    // ==== CADASTRO DE CARTAS ====
-    Carta cartas[32];
-    int i;
+    struct Carta cartas[32];
+    int indice = 0;
 
-    for (i = 0; i < 32; i++)
+    //percorre cada estado para por as letras de "A a "H"
+    for (char estado = 'A'; estado <= 'H'; estado++)
     {
-        // a cada 4 cidades pula pro próximo estado
-        int estadoIndex = i / 4;
-        // pega o resto da divisão, e começa no 0 e vai até o 3 de tipo, cidade 0,1,2,3
-        int cidadeIndex = i % 4;
-        // recebe a letra de tipo, estado A01,A02
-        char estado = 'A' + estadoIndex;
-        // recebe o numero da cidade
-        int numeroCidade = cidadeIndex + 1;
-
-        // gera o codigo da cidade
-        sprintf(cartas[i].codigo, "%c%02d", estado, numeroCidade);
-
-        printf("\nCadastro da carta %s\n", cartas[i].codigo);
-
-        printf("População: ");
-        scanf("%d", &cartas[i].populacao);
-
-        printf("Aréa: ");
-        scanf("%f", &cartas[i].area);
-
-        if (cartas[i].area > 0)
+        for (int cidade = 1; cidade <= 4; cidade++)
         {
-            cartas[i].densidade = cartas[i].populacao / cartas[i].area;
-        }
-        else
-        {
-            cartas[i].densidade = 0;
-        }
+            // cria o código da carta ex (A01)
+            sprintf(cartas[indice].codigo, "%c%02d", estado, cidade);
 
-        printf("PIB: ");
-        scanf("%f", &cartas[i].pib);
+            printf("\n========== CADASTRO DA CARTA %s ==========\n", cartas[indice].codigo);
 
-        if (cartas[i].pib > 0 && cartas[i].populacao > 0)
-        {
-            cartas[i].pibPerCapita = cartas[i].pib / cartas[i].populacao;
+            // recebe do usúario a quantidade da população
+            printf("\nPopulação: ");
+            scanf("%d", &cartas[indice].populacao);
+
+            // recebe do usúario o tamanho da área
+            printf("Área: ");
+            scanf("%f", &cartas[indice].area);
+
+            printf("PIB: ");
+            scanf("%f", &cartas[indice].pib);
+
+            printf("Pontos Turísticos: ");
+            scanf("%d", &cartas[indice].pontosTuristicos);
+
+            indice++;
         }
-        else
-        {
-            cartas[i].pibPerCapita = 0;
-        }
-        printf("Números de Pontos Tuísticos: ");
-        scanf("%d", &cartas[i].pontosTuristicos);
     }
 
-    printf("-------------------\n cartas super trunfo \n--------------------");
+    // SAIDA DOS DADOS CADASTRADOS DAS CARTAS
 
-    for (i = 0; i < 32; i++)
+    printf("\n========== CARTAS CADASTRADAS ==========\n");
 
+    //mostras as 32 cartas cadastradas
+    for (int i = 0; i < 32; i++)
     {
-        printf("Carta %s:\n", cartas[i].codigo);
-        printf(" População: %d\n", cartas[i].populacao);
-        printf("  Área: %.2f\n", cartas[i].area);
-        printf("  Pontos Turísticos: %d\n", cartas[i].pontosTuristicos);
-        printf("  PIB: %.2f\n", cartas[i].pib);
-        printf("  Densidade Populacional: %.2f hab/km^2\n", cartas[i].densidade);
-        printf("  PIB per Capita: %.2f\n", cartas[i].pibPerCapita);
-        printf("--------------------------------------\n");
+        printf("\n========= Carta %s =========\n", cartas[i].codigo);
+        printf("\n População: %d\n----------------------", cartas[i].populacao);
+        printf("\n Área: %.2f\n----------------------", cartas[i].area);
+        printf("\n PIB: %.2f\n----------------------", cartas[i].pib);
+        printf("\n Pontos Turísticos: %d\n----------------------", cartas[i].pontosTuristicos);
     }
 
     return 0;
